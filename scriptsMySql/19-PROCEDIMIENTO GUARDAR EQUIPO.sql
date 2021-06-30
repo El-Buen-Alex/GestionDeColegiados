49a399243746015d50a3893fcb24b700aadd4de6
Darwin <alex98drn@gmail.com>
2021-06-29 23:19:20 -0500

    Creacion de tabla equipo y creacion de procedimiento para guardar un equipo

---
+++ scriptsMySql/19-PROCEDIMIENTO GUARDAR EQUIPO.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE PROCEDURE guardarEquipo( 
+	in _nombre varchar(25),
+    in _numero_jugadores int,
+    in _nombre_director_tecnico varchar(25),
+    in _presidente_equipo varchar(25))
+		BEGIN 
+					INSERT INTO equipo(nombre,numero_jugadoreS,nombre_director_tecnico,presidente_equipo)
+			VALUES	(_nombre, _numero_jugadores, _nombre_director_tecnico,  _presidente_equipo);
+            
+			END$$
+DELIMITER 
+    
