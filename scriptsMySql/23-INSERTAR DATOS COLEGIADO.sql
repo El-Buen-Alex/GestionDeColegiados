76842f4612b8bb958ddd6827d850674bce4ae095
AnthonyDelValle <aadvl382016@outlook.com>
2021-07-06 11:12:01 -0500

    Datos colegiados(commit)

---
+++ scriptsMySql/23-INSERTAR DATOS COLEGIADO.sql
@@ -0,0 +1,60 @@
+INSERT INTO juezcentral (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('0123456789','Eduardo','Cruz','Guayaquil','eduardo@hotmail.com','0923422354');
+INSERT INTO asistente1 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('2334125432','Luis','Alvaro','Guayaquil','luis@hotmail.com','0923452354','Derecha');
+INSERT INTO asistente2 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('4562444575','Juan','SS','Guayaquil','juan@hotmail.com','0912435634','Izquierda');
+INSERT INTO cuartoarbitro (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('1243453464','Jose','JJ','Guayaquil','jose@hotmail.com','5673435554');
+INSERT INTO colegiado (idjuezcentral,idasistente1,idasistente2,idcuartoarbitro) 
+        VALUES ('1','1','1','1');
+INSERT INTO juezcentral (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('1111111111','AAAAA','Cruz','Guayaquil','AAAAA@hotmail.com','0923422354');
+INSERT INTO asistente1 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('2222222222','Maria','Cruz','Guayaquil','Maria@hotmail.com','0923422354','Derecha');
+INSERT INTO asistente2 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('3333333333','Andres','Cruz','Guayaquil','Andres@hotmail.com','0923422354','Izquierda');
+INSERT INTO cuartoarbitro (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('4444444444','Armado','Suarez','Guayaquil','Armado@hotmail.com','0923422354');
+INSERT INTO colegiado (idjuezcentral,idasistente1,idasistente2,idcuartoarbitro) 
+        VALUES ('2','2','2','2');
+INSERT INTO juezcentral (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('5555555555','Cecilia','Cruz','Guayaquil','Cecilia@hotmail.com','0923422354');
+INSERT INTO asistente1 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('6666666666','Fabian','Cruz','Guayaquil','Fabian@hotmail.com','0923422354','Derecha');
+INSERT INTO asistente2 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('7777777777','Fernando','Cruz','Guayaquil','Fernando@hotmail.com','0923422354','Izquierda');
+INSERT INTO cuartoarbitro (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('8888888888','Eivor','Matalobos','Guayaquil','Eivor@hotmail.com','0923422354');
+INSERT INTO colegiado (idjuezcentral,idasistente1,idasistente2,idcuartoarbitro) 
+        VALUES ('3','3','3','3');
+INSERT INTO juezcentral (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('9999999999','Ezio','Auditore','Guayaquil','Ezio@hotmail.com','0923422354');
+INSERT INTO asistente1 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('1212121212','Edward','Kenway','Guayaquil','Edward@hotmail.com','0923422354','Derecha');
+INSERT INTO asistente2 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('3434343434','Altair','Ibn-La Ahad','Guayaquil','Altair@hotmail.com','0923422354','Izquierda');
+INSERT INTO cuartoarbitro (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('5656565656','Connor','Kenway','Guayaquil','Connor@hotmail.com','0923422354');
+INSERT INTO colegiado (idjuezcentral,idasistente1,idasistente2,idcuartoarbitro) 
+        VALUES ('4','4','4','4');
+INSERT INTO juezcentral (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('7878787878','Arno','Dorian','Guayaquil','Arno@hotmail.com','0923422354');
+INSERT INTO asistente1 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('9090909009','Desmond','Miles','Guayaquil','Desmond@hotmail.com','0923422354','Derecha');
+INSERT INTO asistente2 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('1313131313','Shay Patrick','Cormac','Guayaquil','Shay@hotmail.com','0923422354','Izquierda');
+INSERT INTO cuartoarbitro (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('1414141414','Bayek','de Siwa','Guayaquil','Bayek@hotmail.com','0923422354');
+INSERT INTO colegiado (idjuezcentral,idasistente1,idasistente2,idcuartoarbitro) 
+        VALUES ('5','5','5','5');
+INSERT INTO juezcentral (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('1515151515','Jacob','Frye','Guayaquil','Jacob@hotmail.com','0923422354');
+INSERT INTO asistente1 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('1616161616','Evie','Frye','Guayaquil','Evie@hotmail.com','0923422354','Derecha');
+INSERT INTO asistente2 (cedula,nombre,apellido,domicilio,email,telefono,banda) 
+        VALUES ('1717171717','Adewalé','Jackdaw','Guayaquil','Adewalé@hotmail.com','0923422354','Izquierda');
+INSERT INTO cuartoarbitro (cedula,nombre,apellido,domicilio,email,telefono) 
+        VALUES ('1818181818','Aveline','de Grandpré','Guayaquil','Aveline@hotmail.com','0923422354');
+INSERT INTO colegiado (idjuezcentral,idasistente1,idasistente2,idcuartoarbitro) 
+        VALUES ('6','6','6','6');
