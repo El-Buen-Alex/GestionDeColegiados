73ed15fda3b1029a3605413a5becd052d654740a
Daniel Pincay <alexispincay005@gmail.com>
2021-07-06 12:32:35 -0500

    Ventana Interfaz - Gestión Encuentro P1
    Se ha realizado el diseño de la interfaz grafica de las gestiones de encuentro

--- GestionDeColegiados/GestionDeColegiados/Properties/Resources.Designer.cs
+++ GestionDeColegiados/GestionDeColegiados/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace GestionDeColegiados.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -83,6 +83,16 @@ namespace GestionDeColegiados.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Local {
+            get {
+                object obj = ResourceManager.GetObject("Local", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap login_rounded_right {
             get {
                 object obj = ResourceManager.GetObject("login_rounded_right", resourceCulture);
@@ -116,6 +126,16 @@ namespace GestionDeColegiados.Properties {
         internal static System.Drawing.Bitmap Min {
             get {
                 object obj = ResourceManager.GetObject("Min", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap visitante {
+            get {
+                object obj = ResourceManager.GetObject("visitante", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
--- GestionDeColegiados/GestionDeColegiados/frmAsignarGrupoColegiados.Designer.cs
+++ GestionDeColegiados/GestionDeColegiados/frmAsignarGrupoColegiados.Designer.cs
@@ -29,22 +29,190 @@ namespace GestionDeColegiados
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEquipoLocal = new System.Windows.Forms.Label();
+            this.lblEquipoVisitante = new System.Windows.Forms.Label();
+            this.registrar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtpFechaEncuentro = new System.Windows.Forms.DateTimePicker();
+            this.cmbGrupoColegiado = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(38, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(517, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AIGNACIÓN DE GRUPO DE COLEGIADOS \r\nA UN ENCUENTRO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GestionDeColegiados.Properties.Resources.Local;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 145);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GestionDeColegiados.Properties.Resources.visitante;
+            this.pictureBox2.Location = new System.Drawing.Point(334, 145);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(242, 135);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Impact", 27F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(263, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 56);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "VS";
+            // 
+            // lblEquipoLocal
+            // 
+            this.lblEquipoLocal.AutoSize = true;
+            this.lblEquipoLocal.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblEquipoLocal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipoLocal.Location = new System.Drawing.Point(114, 199);
+            this.lblEquipoLocal.Name = "lblEquipoLocal";
+            this.lblEquipoLocal.Size = new System.Drawing.Size(0, 20);
+            this.lblEquipoLocal.TabIndex = 6;
+            // 
+            // lblEquipoVisitante
+            // 
+            this.lblEquipoVisitante.AutoSize = true;
+            this.lblEquipoVisitante.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblEquipoVisitante.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipoVisitante.Location = new System.Drawing.Point(430, 199);
+            this.lblEquipoVisitante.Name = "lblEquipoVisitante";
+            this.lblEquipoVisitante.Size = new System.Drawing.Size(0, 20);
+            this.lblEquipoVisitante.TabIndex = 7;
+            // 
+            // registrar
+            // 
+            this.registrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.registrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.registrar.Location = new System.Drawing.Point(410, 530);
+            this.registrar.Name = "registrar";
+            this.registrar.Size = new System.Drawing.Size(145, 49);
+            this.registrar.TabIndex = 11;
+            this.registrar.Text = "SIGUIENTE";
+            this.registrar.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(225, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 49);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "REGISTRAR";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // dtpFechaEncuentro
+            // 
+            this.dtpFechaEncuentro.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaEncuentro.Location = new System.Drawing.Point(318, 334);
+            this.dtpFechaEncuentro.Name = "dtpFechaEncuentro";
+            this.dtpFechaEncuentro.Size = new System.Drawing.Size(200, 22);
+            this.dtpFechaEncuentro.TabIndex = 13;
+            // 
+            // cmbGrupoColegiado
+            // 
+            this.cmbGrupoColegiado.FormattingEnabled = true;
+            this.cmbGrupoColegiado.Location = new System.Drawing.Point(318, 386);
+            this.cmbGrupoColegiado.Name = "cmbGrupoColegiado";
+            this.cmbGrupoColegiado.Size = new System.Drawing.Size(121, 24);
+            this.cmbGrupoColegiado.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(75, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Fecha de Encuentro:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(76, 386);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Grupo de Colegiado:";
             // 
             // frmAsignarGrupoColegiados
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(581, 466);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(129)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(595, 624);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbGrupoColegiado);
+            this.Controls.Add(this.dtpFechaEncuentro);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.registrar);
+            this.Controls.Add(this.lblEquipoVisitante);
+            this.Controls.Add(this.lblEquipoLocal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmAsignarGrupoColegiados";
             this.Text = "frmNuevoGrupoColegiados";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEquipoLocal;
+        private System.Windows.Forms.Label lblEquipoVisitante;
+        private System.Windows.Forms.Button registrar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dtpFechaEncuentro;
+        private System.Windows.Forms.ComboBox cmbGrupoColegiado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
